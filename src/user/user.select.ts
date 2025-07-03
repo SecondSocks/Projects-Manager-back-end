2d5160d83c29106b8f29bfb55a7a8e4a742a6a3f
SecondSocks <159525526+SecondSocks@users.noreply.github.com>
2025-07-03 15:14:01 +0700

    Добавить функции управления пользователями и DTO для создания пользователя

---
+++ src/user/user.select.ts
@@ -0,0 +1,15 @@
+export const UserProfileSelect = {
+	id: false,
+	email: true,
+	name: true,
+	surname: true,
+	createdAt: false,
+	updatedAt: false,
+	password: true,
+	avatar: true,
+	age: true,
+	phoneNumber: true,
+	aboutMe: true,
+	country: true,
+	isAdmin: false
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,20 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	NotFoundException
+} from '@nestjs/common'
 import { User } from 'generated/prisma'
 import { RegisterDto } from 'src/auth/dto/auth.dto'
 import { PrismaService } from 'src/prisma.service'
+import { UserCreateDto } from './dto/user.dto'
+import { UserProfileSelect } from './user.select'
 
 @Injectable()
 export class UserService {
 	constructor(private readonly prisma: PrismaService) {}
 
+	// For all users
 	async getByEmail(email: string): Promise<User> {
 		const user = await this.prisma.user.findUnique({
 			where: { email }
@@ -27,11 +35,80 @@ export class UserService {
 		return user
 	}
 
+	async getProfile(userId: string): Promise<User> {
+		const user = await this.prisma.user.findUnique({
+			where: { id: userId },
+			select: UserProfileSelect
+		})
+
+		if (!user) throw new NotFoundException('User not found')
+
+		return user
+	}
+
 	async create(data: RegisterDto): Promise<User> {
 		const user = await this.prisma.user.create({
 			data
 		})
 
 		return user
+	}
+
+	async updateProfile(userId: string, data: Partial<User>): Promise<User> {
+		const user = await this.prisma.user.update({
+			where: { id: userId },
+			data,
+			select: UserProfileSelect
+		})
+
+		if (!user) throw new NotFoundException('User not found')
+
+		return user
+	}
+
+	async delete(userId: string): Promise<boolean> {
+		const user = await this.prisma.user.delete({
+			where: { id: userId }
+		})
+
+		if (!user) throw new NotFoundException('User not found')
+
+		return true
+	}
+
+	// For admin
+	async getAllUsers(): Promise<User[]> {
+		const users = await this.prisma.user.findMany()
+
+		if (!users || users.length === 0)
+			throw new NotFoundException('No users found')
+
+		return users
+	}
+
+	async updateUser(userId: string, data: Partial<User>): Promise<User> {
+		const user = await this.prisma.user.update({
+			where: { id: userId },
+			data
+		})
+
+		if (!user) throw new NotFoundException('User not found')
+
+		return user
+	}
+
+	async createUser(data: UserCreateDto): Promise<User['id']> {
+		const oldUser: User = await this.getByEmail(data.email)
+
+		if (oldUser)
+			throw new BadRequestException('User already exists with this email')
+
+		const user: User = await this.prisma.user.create({
+			data
+		})
+
+		if (!user) throw new InternalServerErrorException('User creation failed')
+
+		return user.id
 	}
 }
